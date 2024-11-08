445ba458adc2b864c8b0bf8dc9bdb5243e657ac3
workplacePJ <workplace.pj@proton.me>
2024-11-08 13:27:17 +0900

    Update company.py

--- api/routers/company.py
+++ api/routers/company.py
@@ -2,22 +2,22 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/company/list")
+@router.get('/company/list')
 async def list_company():
     pass
     
-@router.get("/company/{company_id}")
+@router.get('/company/{company_id}')
 async def detail_company():
     pass
 
-@router.post("/company/add")
+@router.post('/company/add')
 async def add_company():
     pass
 
-@router.put("/company/{company_id}")
+@router.put('/company/{company_id}')
 async def edit_company():
     pass
 
-@router.delete("/company/{company_id}")
+@router.delete('/company/{company_id}')
 async def remove_company():
     pass
