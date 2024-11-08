7233340564aa6eaebfa5957c5d503dea24056cca
workplacePJ <workplace.pj@proton.me>
2024-11-08 14:22:54 +0900

    Update company.py

--- api/routers/company.py
+++ api/routers/company.py
@@ -6,7 +6,7 @@ router = APIRouter()
 
 @router.get('/company/list', response_model = List[company_schema.Company])
 async def list_company():
-    return [company_schema.Company(id = 1, name = '株式会社トータルメディエイト', postal_code = '114-0052', address = '東京都北区上十条2-25-4', building_name = '榎本ビル', floor = '所在階', tel = '03-5963-4880', fax = '03-5963-4881')]
+    return [company_schema.Company(id = 1, name = '株式会社トータルメディエイト', postal_code = '114-0052', address = '東京都北区上十条2-25-4', building_name = '榎本ビル', room_number = '所在階', tel = '03-5963-4880', fax = '03-5963-4881')]
     
 @router.get('/company/{company_id}')
 async def detail_company():
