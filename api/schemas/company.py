808799b634b86cf17e19656ca5486a2ede15ba19
workplacePJ <workplace.pj@proton.me>
2024-11-08 15:17:58 +0900

    Update company.py

--- api/schemas/company.py
+++ api/schemas/company.py
@@ -1,11 +1,16 @@
 from pydantic import BaseModel, Field
+from typing import Optional
 
 class Company(BaseModel):
     id: int
     name: str
     postal_code: str
     address: str
-    building_name: str | None = None
-    room_number: str | None = None
+    building_name: Optional[str]
+    room_number: Optional[str]
     tel: str
     fax: str
+    #mail: str
+    #HP: str
+    invoice_registration_number: str
+    #company_seal: str
