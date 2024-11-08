5df5ce0eabdeaa477e8cb111e122153e992cebcd
workplacePJ <workplace.pj@proton.me>
2024-11-08 13:25:30 +0900

    Update company.py

--- api/schemas/company.py
+++ api/schemas/company.py
@@ -1,1 +1,15 @@
+from pydantic import BaseModel, Field
 
+class Company(BaseModel):
+    id: int
+    name: str = Field(None, description='名称')
+    done: bool = Field(False, description="完了フラグ")
+  
+    'postal_code': '114-0034',
+    'address': '東京都北区上十条2-25-4 榎本ビル1F',
+    'tel': '03-5963-4880',
+    'fax': '03-5963-4881',
+    'mail': None,
+    'HP': None,
+    'company_seal': None,
+    'invoice_registration_number': None,
