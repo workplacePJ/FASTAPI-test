a1bfb20ee900849efed75fbff897833d01d52de8
workplacePJ <workplace.pj@proton.me>
2024-11-08 14:04:57 +0900

    Update company.py

--- api/routers/company.py
+++ api/routers/company.py
@@ -3,12 +3,9 @@ import api.schemas.company as company_schema
 
 router = APIRouter()
 
-@router.get('/company/list')
+@router.get('/company/list', response_model = List[company_schema.Company])
 async def list_company():
-    return [company_schema.Company(id=1, title="")]
-    
-@router.get("/tasks", response_model=List[task_schema.Task])
-async def list_tasks():
+    return [company_schema.Company(id = 1, name = '株式会社トータルメディエイト', postal_code = '114-0052', address = '東京都北区上十条2-25-4', building_name = '榎本ビル', floor = '所在階', tel = '03-5963-4880', fax = '03-5963-4881')]
     
 @router.get('/company/{company_id}')
 async def detail_company():
