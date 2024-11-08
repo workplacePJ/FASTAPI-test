4adec9abf2a63000da59f0ca014a2fb1b699958c
workplacePJ <workplace.pj@proton.me>
2024-11-08 15:37:51 +0900

    Update company.py

--- api/schemas/company.py
+++ api/schemas/company.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, Field
 class Company(BaseModel):
     id: int
     name: str
-    postal_code: str
+    postal_code: str = Field(..., regex = r'^[0-9]{3}-[0-9]{4}$')
     address: str
     building_name: str | None = None
     room_number: str | None = None
